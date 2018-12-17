945090d6a564bcf9a9a613ea64c648c2a3e6e1fa
Tu707rki <42980218+Tu707rki@users.noreply.github.com>
2018-12-16 18:38:32 -0600

    Add files via upload

---
+++ Final Turki/Final Turki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final Turki
+//
+//  Created by cyberdev on 12/15/18.
+//  Copyright © 2018 cyberdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
